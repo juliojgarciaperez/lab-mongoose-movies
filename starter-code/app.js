7659172ae4ecd71561acbd2e29f930c43cf6c84b
Julio García Pérez <juliojose.garcia@11paths.com>
2019-01-12 14:07:42 +0100

    foo

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,7 +16,7 @@ mongoose
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongoDB', err)
   });
 
 const app_name = require('./package.json').name;
